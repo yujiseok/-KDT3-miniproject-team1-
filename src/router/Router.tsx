aafaaad8f0987e0a7e0e7c53674830c99f8f78f6
yujiseok <koesijuy@gmail.com>
2023-02-13 22:37:51 +0900

    Feat: 유저 페이지 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -15,6 +15,7 @@ import SingUp from "pages/SingUp";
 import Search from "pages/Search";
 import NotFound from "pages/NotFound";
 import Order from "pages/Order";
+import User from "pages/User";
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,6 +28,7 @@ const Router = createBrowserRouter(
         <Route path="/search/:value" element={<Search />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/mypage" element={<MyPage />} />
+        <Route path="/user" element={<User />} />
         <Route path="/order" element={<Order />} />
         <Route path="/likes" element={<Likes />} />
       </Route>
