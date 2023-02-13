d4ad543b7b70f899ff1fb6e50aa9594b08a51962
yujiseok <koesijuy@gmail.com>
2023-02-13 17:50:15 +0900

    Feat: 색 추가

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,1 +1,15 @@
-export {};
+const colors = {
+  "INDIGO-0": "#EDF2FF",
+  "INDIGO-1": "#DBE4FF",
+  "INDIGO-2": "#BAC8FF",
+  "INDIGO-3": "#91A7FF",
+  "INDIGO-4": "#748FFC",
+  "INDIGO-5": "#5C7CFA",
+  "INDIGO-6": "#4C6EF5",
+  "INDIGO-7": "#4263EB",
+  "INDIGO-8": "#3B5BDB",
+  "INDIGO-9": "#364FC7",
+  "GRAY-9": "#212529",
+};
+
+export default colors;
