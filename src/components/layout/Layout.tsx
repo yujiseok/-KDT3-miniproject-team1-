6cd752a939b6030b3ccc56d688db9ea9f89b50a5
yujiseok <koesijuy@gmail.com>
2023-02-15 15:37:28 +0900

    Feat: 메인 태그 패딩추가, 폰트 글로벌로 관리

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,21 @@
 import Footer from "components/Footer";
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 const Layout = () => {
   return (
     <>
       <header>헤더</header>
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
       <Footer />
     </>
   );
 };
+
+const Main = styled.main`
+  padding: 0 12px;
+`;
+
 export default Layout;
