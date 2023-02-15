ff72b174d002e550a4e511a87cad882bae47fc29
plou102 <plou102@naver.com>
2023-02-15 21:11:17 +0900

    Feat: 헤더 디자인 및 링크 설정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,11 @@
 import Footer from "components/Footer";
+import Header from "components/Header";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
-      <header>헤더</header>
+      <Header />
       <main>
         <Outlet />
       </main>
