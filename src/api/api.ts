6a596dd89495804423ae108b068a9ab3a1b9fcfc
yujiseok <koesijuy@gmail.com>
2023-02-21 21:34:48 +0900

    Feat: axios baseURL 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import type { AxiosRequestConfig } from "axios";
 import axios from "axios";
 
 const config: AxiosRequestConfig = {
-  baseURL: "",
+  baseURL: "http://15.164.195.118:3100/",
 };
 
 export const client = axios.create(config);
