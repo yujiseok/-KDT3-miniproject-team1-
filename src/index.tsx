b8477578a2b0dd77b83bfa021e2cbbb26d153831
yujiseok <koesijuy@gmail.com>
2023-02-21 21:31:30 +0900

    Feat: react query provider, devtool 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { store } from "./app/store";
 import App from "./App";
+
+const queryClient = new QueryClient();
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
 
 root.render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </Provider>,
 );
