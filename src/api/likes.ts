721e58523a859ab2b2253c94c64c8e7a2c3829a8
yujiseok <koesijuy@gmail.com>
2023-02-23 21:09:28 +0900

    Feat: 관심상품 신청내역 api 작업중

--- src/api/likes.ts
+++ src/api/likes.ts
@@ -1,20 +1,21 @@
 import { client } from "./api";
 
+const headers = {
+  // accept: "application/json;charset=UTF-8",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcImFjY291bnROb25FeHBpcmVkXCI6ZmFsc2UsXCJjcmVkZW50aWFsc05vbkV4cGlyZWRcIjpmYWxzZSxcImFjY291bnROb25Mb2NrZWRcIjpmYWxzZSxcInBhc3N3b3JkXCI6bnVsbCxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwiYXV0aG9yaXRpZXNcIjpudWxsfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MTUzNzE3LCJleHAiOjE2NzcxNTczMTd9.aNmuG2EjeY3KKMFJgDkCmMkIfQ8L7P6ynjszU-xmuD0",
+};
 export const getLikeLists = async () => {
   const { data } = await client.get("/like", {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MDcwMjY5LCJleHAiOjE2NzcxMDYyNjl9.9bJfszToLmXfW7q6zrG_KeMNu5FwfPotQiE3qXGBYHY`,
-    },
+    headers,
   });
 
   return data.data;
 };
 
 export const postLikeLists = async (productId: string) => {
-  const { data } = await client.post(`/like`, {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MDY3Mzk5LCJleHAiOjE2NzcxMDMzOTl9.hgFtFx93_dgZkCWepxsspV0MdvJd9jfKohgBrg2hRIw`,
-    },
+  const { data } = await client.post("/like", {
+    headers,
     data: {
       productId,
     },
@@ -24,14 +25,14 @@ export const postLikeLists = async (productId: string) => {
 };
 
 export const deleteLikeList = async (id: number) => {
-  const { data } = await client.delete(`/like`, {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MDY3Mzk5LCJleHAiOjE2NzcxMDMzOTl9.hgFtFx93_dgZkCWepxsspV0MdvJd9jfKohgBrg2hRIw`,
-    },
+  const { data } = await client.delete("/like", {
+    headers,
     data: {
       id,
     },
   });
 
-  return data.data;
+  console.log(data);
+
+  return data;
 };
--- src/api/orders.ts
+++ src/api/orders.ts
@@ -1,10 +1,14 @@
 import { client } from "./api";
 
+const headers = {
+  accept: "application/json;charset=UTF-8",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MTQ4NjE2LCJleHAiOjE2NzcxNTIyMTZ9.pRCev6i5JInuR9Fo0lpXiLjjIncrvNKcndfTjKq97Ig",
+};
+
 export const getOrderLists = async () => {
   const { data } = await client.get("/order", {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MDcwMjY5LCJleHAiOjE2NzcxMDYyNjl9.9bJfszToLmXfW7q6zrG_KeMNu5FwfPotQiE3qXGBYHY`,
-    },
+    headers,
   });
 
   return data.data;
@@ -13,9 +17,7 @@ export const getOrderLists = async () => {
 // 바디로
 export const postOrder = async (productId: string) => {
   const { data } = await client.post(`/order`, {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MDY3Mzk5LCJleHAiOjE2NzcxMDMzOTl9.hgFtFx93_dgZkCWepxsspV0MdvJd9jfKohgBrg2hRIw`,
-    },
+    headers,
     data: {
       productId,
     },
@@ -26,10 +28,8 @@ export const postOrder = async (productId: string) => {
 
 // 쿼리로
 export const deleteOrder = async (cartId: number) => {
-  const { data } = await client.delete(`/order/${cartId}`, {
-    headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ7XCJ1c2VySWRcIjoxLFwiZW1haWxcIjpcInRlc3RAZW1haWwuY29tXCIsXCJqd3RUeXBlXCI6XCJBQ0NFU1NcIixcImVuYWJsZWRcIjpmYWxzZSxcInVzZXJuYW1lXCI6XCJ0ZXN0QGVtYWlsLmNvbVwiLFwicGFzc3dvcmRcIjpudWxsLFwiYXV0aG9yaXRpZXNcIjpudWxsLFwiYWNjb3VudE5vbkV4cGlyZWRcIjpmYWxzZSxcImNyZWRlbnRpYWxzTm9uRXhwaXJlZFwiOmZhbHNlLFwiYWNjb3VudE5vbkxvY2tlZFwiOmZhbHNlfSIsImlzcyI6IjEgdGVhbSBiYWNrZW5kIiwiaWF0IjoxNjc3MDY3Mzk5LCJleHAiOjE2NzcxMDMzOTl9.hgFtFx93_dgZkCWepxsspV0MdvJd9jfKohgBrg2hRIw`,
-    },
+  const { data } = await client.delete(`/order/?basketId=${cartId}`, {
+    headers,
   });
 
   return data.data;
