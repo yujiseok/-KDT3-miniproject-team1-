d892c192a2b8ce0f1a84faf415a70d966fd126c6
plou102 <plou102@naver.com>
2023-02-24 20:42:08 +0900

    Feat: 오류 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -64,21 +64,21 @@ const Main = () => {
 
   useEffect(() => {
     function Recommend() {
-      if (authInfo.joinType === 1) {
+      if (authInfo[0]?.joinType === 1) {
         setRecommend(recommend1.splice(0, 5));
-      } else if (authInfo.joinType === 2) {
+      } else if (authInfo[0]?.joinType === 2) {
         setRecommend(recommend2.splice(0, 5));
-      } else if (authInfo.joinType === 3) {
+      } else if (authInfo[0]?.joinType === 3) {
         setRecommend(recommend3.splice(0, 5));
-      } else if (authInfo.joinType === 4) {
+      } else if (authInfo[0]?.joinType === 4) {
         setRecommend(recommend4.splice(0, 5));
-      } else if (authInfo.joinType === 5) {
+      } else if (authInfo[0]?.joinType === 5) {
         setRecommend(recommend5.splice(0, 5));
       }
     }
     Recommend();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [authInfo.joinType]);
+  }, [authInfo[0]?.joinType]);
 
   if (Object.keys(authInfo).length === 0) {
     return (
@@ -131,7 +131,7 @@ const Main = () => {
       {/* 회원용 */}
       <TitleContent>
         <h2>
-          <span>{authInfo?.name}</span>님이
+          <span>{authInfo[0]?.name}</span>님이
           <br />
           신청하신 대출 정보 입니다.
         </h2>
@@ -163,7 +163,7 @@ const Main = () => {
 
       <RecommendContent>
         <h2 className="recommend">
-          <span>{authInfo?.name}</span>님의 맞춤 대출 정보 입니다.
+          <span>{authInfo[0]?.name}</span>님의 맞춤 대출 정보 입니다.
         </h2>
         {recommend.length > 0
           ? recommend.map((item) => {
